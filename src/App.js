da21e098700223b511fb131c0bfeb38c6ae9207a
Paul Launay <launay.p@gmail.com>
2020-02-11 23:40:26 +0100

    feat(initial): initial import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+
 import './App.css';
 
+const editorId = process.env.REACT_APP_EMJPM_EDITOR_ID || 1;
+const editorSecret = process.env.REACT_APP_EMJPM_EDITOR_SECRET || "c1uhbrbyr7d";
+const redirectUrl = process.env.REACT_APP_EMJPM_REDIRECT_URL || "http://localhost:3001";
+const emjpmUrl = process.env.REACT_APP_EMJPM_URL || "https://emjpm.num.social.gouv.fr";
+const emjpmAuthQueryString = queryString.stringify({
+  editor_id: editorId,
+  editor_secret: editorSecret,
+  redirect_url: redirectUrl
+});
+const emjpmAuthUrl = `${emjpmUrl}/application/authorization?${emjpmAuthQueryString}`;
+const emjpmApiUrl = process.env.REACT_API_EMJPM_URL || "http://localhost:4000/api/v2";
+const emjpmApiMesuresUrl = `${emjpmApiUrl}/editors/mesures`;
+
 function App() {
+  const [token, setToken] = useState(null);
+  const [mesures, setMesures] = useState(null);
+
+  useEffect(() => {
+    const localToken = localStorage.getItem('token')
+
+    if (localToken) {
+      setToken(localToken);
+      return;
+    }
+
+    const newToken = queryString.parse(window.location.search).token;
+
+    if (newToken) {
+      localStorage.setItem('token', newToken);
+      window.location.replace(window.location.origin);
+    }
+  }, [setToken]);
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    const fetchMesures = async () => {
+      const res = await fetch(emjpmApiMesuresUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      const { mesures } = await res.json();
+
+      setMesures(mesures);
+    };
+
+    fetchMesures();
+  }, [token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>eMJPM - Application métier</h1>
+      {token ? (
+        <code styles={{ o: "80%"}}>{token}</code>
+      ) : (
+        <a href={emjpmAuthUrl} className="button">
+          Connexion eMJPM
         </a>
-      </header>
+      )}
+      {mesures && (
+        <div>
+          <h4>Mesures</h4>
+          {mesures.map(mesure => (
+            <div key={mesure.id}>
+              <span>#{mesure.id} - {mesure.ville}</span>
+              <span>{mesure.ville}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
