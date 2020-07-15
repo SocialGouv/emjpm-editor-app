0c04d5fc6a40fc82cd3e0cdb4ec81e554cb92174
Rémi Roycourt <remiroycourt@gmail.com>
2020-07-15 16:00:28 +0200

    feat(mesure): apply api modifications

--- src/Mesures.js
+++ src/Mesures.js
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { Box, Flex, Heading } from "rebass";
-import { Button } from "@socialgouv/emjpm-ui-core";
-import { MesureListItem } from "@socialgouv/emjpm-ui-components";
+import useSWR from 'swr';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Box, Flex, Heading } from 'rebass';
+import { Button } from '@socialgouv/emjpm-ui-core';
+import { MesureListItem } from '@socialgouv/emjpm-ui-components';
 
-import { API_URL } from "./constants";
+import { API_URL } from './constants';
 
 function Mesures(props) {
   const { token } = props;
-  const [mesures, setMesures] = useState(null);
 
-  const handleDelete = async (id) => {
-    await fetch(`${API_URL}/editors/mesures/${id}`, {
-      method: "delete",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
-    });
-  };
-
-  useEffect(() => {
-    if (!token) {
-      setMesures(null);
-      return;
-    }
-
-    const fetchMesures = async () => {
-      const res = await fetch(`${API_URL}/editors/mesures?status=Mesure en cours`, {
+  const { data, error, mutate } = useSWR(
+    token ? `${API_URL}/editors/mesures?status=en_cours` : null,
+    (url) =>
+      fetch(url, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      });
+      }).then((r) => r.json())
+  );
 
-      const { mesures } = await res.json();
+  const handleDelete = async (id) => {
+    mutate(
+      { ...data, mesures: data.mesures.filter((m) => m.id !== id) },
+      false
+    );
+    await fetch(`${API_URL}/editors/mesures/${id}`, {
+      method: 'delete',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    mutate();
+  };
 
-      setMesures(mesures);
-    };
-
-    fetchMesures();
-  }, [token]);
-
-  if (!mesures) {
-    return null;
+  if (error) {
+    return <Box>{error}</Box>;
   }
 
+  console.log('data', data);
+
   return (
-    <Box mt="6">
-      <Heading mb="5">Mesures</Heading>
-      {mesures.map(mesure => (
-        <Flex key={mesure.id} alignItems="center">
-          <MesureListItem mesure={mesures} />
-          <Button ml="2" mb="2" onClick={() => handleDelete(mesure.id)}>
-            Supprimer
-          </Button>
-        </Flex>
-      ))}
+    <Box mt='6'>
+      <Heading mb='5'>Mesures</Heading>
+      {data &&
+        data.mesures &&
+        data.mesures.map((mesure) => (
+          <Flex key={mesure.id} alignItems='center'>
+            <MesureListItem mesure={mesure} />
+            <Button ml='2' mb='2' onClick={() => handleDelete(mesure.id)}>
+              Supprimer
+            </Button>
+          </Flex>
+        ))}
     </Box>
   );
 }
