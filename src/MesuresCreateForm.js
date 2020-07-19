0fbf4e6adfed9d323e66f7e1023baed09ab8d8a9
Rémi Roycourt <remiroycourt@gmail.com>
2020-07-19 18:50:34 +0200

    feat(api): champ protection

--- src/MesuresCreateForm.js
+++ src/MesuresCreateForm.js
@@ -37,10 +37,13 @@ yup.setLocale({
   },
   number: {
     integer: 'Le nombre indiqué doit être un entier',
+    max: 'Le nombre indiqué doit être inférieur ou égal à ${max}',
+    min: 'Le nombre indiqué doit être supérieur ou égal à ${min}',
     positive: 'Le nombre indiqué doit être positif',
   },
   string: {
     email: "Le format de l'adresse email n'est pas correct.",
+    length: 'Le champ doit comporter ${length} caractères.',
   },
 });
 
